43ecb03f3f1abd2db82d28bc7e72474d083960d6
Nguyen Louis <nguyenlouis172@gmail.com>
2024-07-13 05:42:06 +0200

    add connexion model

---
+++ models/connexion.php
@@ -0,0 +1,20 @@
+<?php
+
+function  connectBdd()
+{
+    $dbname = "blog";
+    $dbhost = "localhost";
+    $username = "root";
+    $password = "";
+
+    $dns = "mysql:host=$dbhost; dbname= $dbname; charset=utf-8";
+    try {
+        $connexion = new PDO($dns, $username, $password);
+        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $connexion;
+    } catch (PDOException $e) {
+        die('Erreur : ' . $e->getMessage());
+    }
+}
+
+$connexion = connectBdd();

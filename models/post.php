a42ccb65996dba931b2043b9e863269fc1965b67
Nguyen Louis <nguyenlouis172@gmail.com>
2024-07-13 08:08:37 +0200

    add model add posts

--- models/post.php
+++ models/post.php
@@ -10,3 +10,12 @@ function getPosts()
     $results = $request->fetchAll(PDO::FETCH_ASSOC);
     return $results;
 }
+
+function addPosts($title, $content, $image)
+{
+    $date = ('Y-m-d-h-m-i');
+    $connexion = connectBdd();
+    $request = $connexion->prepare("INSERT INTO post (post_id, title, content, image, date ) VALUES (null, ?, ?, ?, ?)");
+    $affectedLines = $request->execute([$title, $content, $image, $date]);
+    return $affectedLines > 0;
+}

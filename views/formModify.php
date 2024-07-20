602f12d88424ad48dc22513ed0b3e82faf2a975e
Nguyen Louis <nguyenlouis172@gmail.com>
2024-07-20 02:06:50 +0200

    add view form to modify post

---
+++ views/formModify.php
@@ -0,0 +1,33 @@
+<?php
+
+$title = "formulaire de modification";
+
+ob_start();
+
+?>
+
+
+<form action="" method="POST" enctype="multipart/form-data">
+    <div class="form-group">
+        <label for="title">title</label>
+
+        <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="form-group">
+        <label for="content">content</label>
+        <textarea class="form-control" id="content" rows="3" name="content"></textarea>
+    </div>
+    <form>
+        <div class="form-group">
+            <label for="image">Image</label>
+            <input type="file" class="form-control-file" id="image" name="file">
+        </div>
+        <div class="form-group">
+            <input type="submit" name="submit">
+        </div>
+    </form>
+
+    <?php
+    $content = ob_get_clean();
+    require_once 'layout.php';
+    ?>

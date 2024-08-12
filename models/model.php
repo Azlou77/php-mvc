be80e6ee35e434522a6638bb9f58a2f3086c7c49
Nguyen Louis <nguyenlouis172@gmail.com>
2024-08-12 03:16:20 +0200

    add function query to model

--- models/model.php
+++ models/model.php
@@ -6,4 +6,28 @@ use App\Database\Connexion;
 
 class Model extends Connexion
 {
+
+    protected $table;
+
+
+    private $connexion;
+
+
+
+    public function query(string $request, array $attributs = null)
+    {
+
+        // Get the instance
+        $this->connexion = Connexion::getPDO();
+
+        // Check if we have attributs
+        if ($attributs !== null) {
+
+            $query = $this->connexion->prepare($request);
+            $query->execute($attributs);
+            return query;
+        } else {
+            return $this->query->execute($request);
+        }
+    }
 }
